709e7d17ce6a71bdcd0b36fe28df53ce0f330626
Hannah Duah <handuah@gmail.com>
2020-07-29 13:06:12 +0100

    added a javascript for the changes in images

---
+++ js/furniture.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    console.log('ready');
+
+    let mainImage = $('#image-placeholder');
+    let firstImage = $('#smaller-image-container1');
+    let secondImage = $('#smaller-image-container2');
+    let thirdImage = $('#smaller-image-container3');
+
+    firstImage.click(function (){
+        mainImage.css('backgroundImage','url(../assets/images/sofa3.jpg)');
+        mainImage.css('transition', 'all 2s');
+    });
+
+    secondImage.click(function (){
+        mainImage.css('backgroundImage','url(../assets/images/sofa4.jpg)');
+        mainImage.css('backgroundSize', 'cover');
+        mainImage.css('transition', 'all 2s');
+    });
+    
+    thirdImage.click(function (){
+        mainImage.css('backgroundImage','url(../assets/images/sofa2.jpg)');
+        mainImage.css('backgroundSize', 'cover');
+        mainImage.css('transition', 'all 2s');
+    });
+});
